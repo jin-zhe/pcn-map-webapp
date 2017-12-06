bd1c001e58aafb5d79216a3ba1ca72f13f060e35
Jin Zhe <spyrant@gmail.com>
2017-12-06 13:37:11 +0800

    add current location marker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import style from './style';
-import { Map, TileLayer, GeoJSON } from 'react-leaflet';
+import { Map, TileLayer, GeoJSON, Marker, Popup } from 'react-leaflet';
 import pcn_geojson from './pcn_geojson.json';
 
 const basemap = 'https://maps-{s}.onemap.sg/v3/Grey/{z}/{x}/{y}.png';
@@ -15,43 +15,46 @@ const maxBounds = [[1.56073, 104.1147], [1.16, 103.502]];
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { currentZoomLevel: zoomLevel };
+    this.state = { 
+      currentZoomLevel: zoomLevel,
+      currentMapCenter:  mapCenter,
+      currentPositionMarker: {
+        show: false,
+        position: mapCenter,
+        accuracy: 2
+      }
+    };
   }
 
   componentDidMount() {
     const leafletMap = this.leafletMap.leafletElement;
-    leafletMap.on('zoomend', () => {
-      const updatedZoomLevel = leafletMap.getZoom();
-      this.handleZoomLevelChange(updatedZoomLevel);
+    leafletMap.locate();
+    leafletMap.on('locationfound', (e) => {
+      this.handleCurrentPositionMarker(true, e.latlng, e.accuracy)
+      this.handleMapCenter(e.latlng); // center to current pos
+      this.handleZoomLevel(18);       // zoom in
+    });
+    leafletMap.on('locationerror', (e) => {
+      alert(e.message);
+      this.handleCurrentPositionMarker(false, 0, 0)
     });
   }
 
-  handleZoomLevelChange(newZoomLevel) {
-    this.setState({ currentZoomLevel: newZoomLevel });
+  handleMapCenter(currentMapCenter) {
+    this.setState({ currentMapCenter: currentMapCenter })
   }
 
-  handleUpPanClick = () => {
-    const leafletMap = this.leafletMap.leafletElement;
-    leafletMap.panBy([0, -100]);
-    window.console.log('Panning up');
+  handleZoomLevel(currentZoomLevel) {
+    this.setState({ currentZoomLevel: currentZoomLevel })
   }
 
-  handleRightPanClick = () => {
-    const leafletMap = this.leafletMap.leafletElement;
-    leafletMap.panBy([100, 0]);
-    window.console.log('Panning right');
-  }
-
-  handleLeftPanClick = () => {
-    const leafletMap = this.leafletMap.leafletElement;
-    leafletMap.panBy([-100, 0]);
-    window.console.log('Panning left');
-  }
-
-  handleDownPanClick = () => {
-    const leafletMap = this.leafletMap.leafletElement;
-    leafletMap.panBy([0, 100]);
-    window.console.log('Panning down');
+  handleCurrentPositionMarker(show, latlng, accuracy) {
+    this.setState({ currentPositionMarker: {
+      show: show,
+      position: latlng,
+      accuracy: accuracy
+      }
+    })
   }
 
   getStyle(feature, layer) {
@@ -64,21 +67,32 @@ class App extends Component {
       'western adventure loop':       '#F99B0C' 
     }
     return {
-      color: colors[feature['properties']['pcn_loop']],
-      weight: 5,
+      color: colors[feature.properties.pcn_loop],
+      weight: 3,
       opacity: 0.88
     }
   }
 
   render() {
-    window.console.log('this.state.currentZoomLevel ->', this.state.currentZoomLevel);
+    function CurrentPositionMarker(props) {
+      if (props.show) {
+        return (
+          <Marker key="currentPos" position={ props.position }>
+            <Popup>
+              <span>{`You are within ${props.accuracy / 2} meters from this point`}</span>
+            </Popup>
+          </Marker>
+        )
+      }
+      return null;
+    }
 
     return (
       <div style={ style.fullHeight }>
         <Map
           ref={m => { this.leafletMap = m; }}
-          center={ mapCenter}
-          zoom={ zoomLevel }
+          center={ this.state.currentMapCenter}
+          zoom={ this.state.currentZoomLevel }
           maxBounds={ maxBounds }
           minZoom={ minZoom }
           maxZoom={ maxZoom }
@@ -89,29 +103,11 @@ class App extends Component {
             url={ basemap }
           />
           <GeoJSON data={ pcn_geojson } style={ this.getStyle } />
+          <CurrentPositionMarker
+            show={ this.state.currentPositionMarker.show }
+            position={ this.state.currentPositionMarker.position }
+            accuracy={ this.state.currentPositionMarker.accuracy} />
         </Map>
-        <div>
-          <div>
-            <div style={{ marginLeft: '37px' }}>
-              <button onClick={ this.handleUpPanClick }>
-                  Pan up
-              </button>
-            </div>
-            <div>
-              <button onClick={ this.handleLeftPanClick }>
-                  Pan left
-              </button>
-              <button onClick={ this.handleRightPanClick }>
-                  Pan right
-              </button>
-            </div>
-            <div style={{ marginLeft: '30px' }}>
-              <button onClick={ this.handleDownPanClick }>
-                  Pan down
-              </button>
-            </div>
-          </div>
-        </div>
       </div>
     );
   }
